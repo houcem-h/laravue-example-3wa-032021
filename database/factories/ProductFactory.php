cd9ba306afeeb3dfcf2a16fad271cb0b40248416
Houcem Hedhly <hedhoucem@gmail.com>
2021-03-05 11:53:39 +0100

    writing factories and seeders

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word(),
+        'category_id' => \App\Category::inRandomOrder()->first()->id,
+        'description' => $faker->paragraph,
+        'price' => rand(1000, 99999),
+        'created_at' => now()
     ];
 });
