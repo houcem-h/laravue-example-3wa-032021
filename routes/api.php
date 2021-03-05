1c0b79227c168bc591c97227d9fc13cc0835b549
Houcem Hedhly <hedhoucem@gmail.com>
2021-03-05 12:35:31 +0100

    creating api's controllers

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('categories', 'Api\CategoryController@index');
+Route::get('products', 'Api\ProductController@index');
